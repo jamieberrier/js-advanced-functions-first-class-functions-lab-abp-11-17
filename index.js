afbdf6892e1e154c5eec96782b82d5cf7da8b3c5
jamieberrier <berrier.jamie@gmail.com>
2017-12-04 05:30:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,7 @@ const createFareMultiplier = function (mulitplier){
 const fareDoubler = createFareMultiplier (2);
 
 const fareTripler = createFareMultiplier (3);
+
+selectDifferentDrivers (drivers, selectingDrivers){
+  return selectingDrivers;
+}
